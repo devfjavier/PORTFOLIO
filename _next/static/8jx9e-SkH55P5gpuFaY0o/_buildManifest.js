da897c64d886a20e6a6613761f8976ed286750d8
javier <devjavicont@gmail.com>
2022-04-27 20:47:22 -0400

    Updates

---
+++ _next/static/8jx9e-SkH55P5gpuFaY0o/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-406a0aa1afd03389.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
